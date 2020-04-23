fd0b7ba433c74a9bb9b1c027bd6da656fd052478
Vipul Anand <va@vipulanand.in>
2020-04-23 17:49:20 +0530

    Functionality demo

---
+++ app.js
@@ -0,0 +1,84 @@
+//fetch json data
+var kurukshetraEssn = fetch('http://127.0.0.1:5500/eMarket-kkr/biz.json').then(post => post.json()).then(post => post.Haryana.Kurukshetra)
+var sec2;
+
+kurukshetraEssn.then(a => sec2 = a.Sector2);
+
+//helper function for getting elements with ID
+var gID = id => {
+    return document.getElementById(id)
+}
+
+var bkrs = gID("bakerCount");
+var bkprs = gID("bookCount");
+var bkpDtl = gID("bks");
+//Select location to fire json
+document.getElementById('locKkr').addEventListener('change', function () {
+    console.log(loc = this.value);
+    return changeLoc()
+});
+var loc;
+var changeLoc = () => {
+    switch (loc) {
+        case "sector2":
+            bkrs.innerText = sec2.bakery.length,
+                bkprs.innerText = sec2.books.length,
+                bkpDtl.innerHTML = `
+                    <div class="shops">
+                    <h5>${sec2.books[0].businessName}</h5>
+                    <span class="feedback">Feedback</span>
+                    <p class="bkp_dtls">
+                    <span class="shopDtls">${sec2.books[0].deliveryMode}</span><br>
+                    <span class="shopDtls">Open ${sec2.books[0].workDuration}</span><br>
+                    </p>
+                    </div>
+                    <div class="homeDelivery">
+                    <span class="shopDtls contact">${sec2.books[0].contact}</span>
+                    </div>
+            `;
+            break;
+        case "sector7":
+            console.log("Sector 7 Shops data parsed from fetched json");
+            break;
+        case "mohannagar":
+            console.log("Mohannagar Shops data parsed from fetched json");
+            break;
+        case "sector13":
+            console.log("Sector 13 Shops data parsed from fetched json");
+            break;
+        case "sector4":
+            console.log("Sector 4 Shops data parsed from fetched json");
+            break;
+        case "sector30":
+            console.log("Sector 30 Shops data parsed from fetched json");
+            break;
+        case "selectLocation":
+            bkrs.innerText = '',
+            bkprs.innerText = '',
+            bkpDtl.innerHTML = '';
+            break;
+        default:
+            console.log("No shop Data for the lcoation")
+    }
+}
+
+
+
+//result UI
+var sd = document.querySelectorAll(".srvc")
+var [selectLoc, khalsa, bkr, kitab, bks, davai, med, sabji, veg, kiryana, grc] = sd
+
+var catClick = (cat, sub) => {
+    cat.addEventListener('click', () => {
+        sub.classList.toggle("show")
+    })
+}
+
+catClick(khalsa, bkr)
+catClick(kitab, bks)
+catClick(davai, med)
+catClick(sabji, veg)
+catClick(kiryana, grc)
+
+
+console.log(kurukshetraEssn)
