f0381854ce1071fe779a0b64e555238d9aaef2cf
Vipul Anand <va@vipulanand.in>
2020-04-24 12:02:48 +0530

    groceries added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//Author: Vipul Anand
 //fetch json data
 var kurukshetraEssn = fetch('https://raw.githubusercontent.com/anandvip/eMarket-kkr/master/biz.json').then(post => post.json()).then(post => post.Haryana.Kurukshetra)
 var sec2;
 
 kurukshetraEssn.then(a => sec2 = a.Sector2);
+
 
 //helper function for getting elements with ID
 var gID = id => {
@@ -11,17 +13,65 @@ var gID = id => {
 
 var bkrs = gID("bakerCount"),
     bkrr = gID("bkr"),
-    bkprs = gID("bookCount"), 
+    bkrD = [],
+    bkprs = gID("bookCount"),
+    bkpDtl = gID("bks"), 
+    bk   = [],
     chem = gID("chemCount"),
     meds = gID("med"),
-    vege = gID("vegCount"),
-    groc = gID("grocerCount"),
-    bkpDtl = gID("bks"),
-    locKkr = gID('locKkr'),
-    loc,
-    bk = [],
     medi = [],
-    bkrD = [];
+    vege = gID("vegCount"),
+    frut = gID("veg"),
+    fru  = [], 
+    groc = gID("grocerCount"),
+    gr = gID("grc"),
+    grr = [],
+    locKkr = gID('locKkr'),
+    loc;
+
+function grcrData(tmplt) {
+    for(var i = 0; i < sec2.groceries.length;i++){
+        
+        tmplt[i] =   `<div class="shops">
+           <h5>${sec2.groceries[i].businessName}</h5>
+           <span class="feedback">Feedback</span>
+           <p class="shopData">
+           <span class="shopDtls">${sec2.groceries[i].deliveryMode}</span><br>
+           <span class="shopDtls">Open ${sec2.groceries[i].workDuration}</span><br>
+           <span class="shopPay">${sec2.groceries[i].paymentAccepted}</span>
+           </p>
+           </div>
+           <div class="homeDelivery">
+           <span class="shopDtls contact"><span class="wa"></span><a href="https://wa.me/${sec2.groceries[i].contact}">${sec2.groceries[i].contact}</a></span>
+           <span class="minOrder">Minimum Order: ${sec2.groceries[i].minimumOrder}</span>
+           </div>`
+           grr.push(tmplt[i])
+       }
+return grr 
+
+}
+
+    function vegData(tmplt) {
+        for(var i = 0; i < sec2.fruits.length;i++){
+        
+            tmplt[i] =   `<div class="shops">
+               <h5>${sec2.fruits[i].businessName}</h5>
+               <span class="feedback">Feedback</span>
+               <p class="shopData">
+               <span class="shopDtls">${sec2.fruits[i].deliveryMode}</span><br>
+               <span class="shopDtls">Open ${sec2.fruits[i].workDuration}</span><br>
+               <span class="shopPay">${sec2.fruits[i].paymentAccepted}</span>
+               </p>
+               </div>
+               <div class="homeDelivery">
+               <span class="shopDtls contact"><span class="wa"></span><a href="https://wa.me/${sec2.fruits[i].contact}">${sec2.fruits[i].contact}</a></span>
+               <span class="minOrder">Minimum Order: ${sec2.fruits[i].minimumOrder}</span>
+               </div>`
+               fru.push(tmplt[i])
+           }
+return fru 
+
+    }
 
     function chemistdata(tmplt){
         for(var i = 0; i < sec2.chemist.length;i++){
@@ -89,7 +139,6 @@ return bkrD
 //Select location to fire json
 locKkr.addEventListener('change', ()=>{
     loc = locKkr.value;
-    
     return changeLoc()
 });
 
@@ -103,6 +152,8 @@ var changeLoc = () => {
                 groc.innerText = sec2.groceries.length,
                 bkpDtl.innerHTML = booksData(sec2.books),
                 bkrr.innerHTML = bakerData(sec2.bakery),
+                frut.innerHTML = vegData(sec2.fruits),
+                gr.innerHTML = grcrData(sec2.groceries),
                 meds.innerHTML = chemistdata(sec2.chemist)
                 ;        
             break;
@@ -120,6 +171,8 @@ var changeLoc = () => {
             groc.innerText   =   '',
             bkrr.innerHTML   =   '',
             meds.innerHTML   =   '',
+            frut.innerHTML   =   '',
+            gr.innerHTML     =   '',
             bkpDtl.innerHTML =   '';
             break;
         default:
@@ -133,6 +186,7 @@ var changeLoc = () => {
 var sd = document.querySelectorAll(".srvc")
 var [selectLoc, khalsa, bkr, kitab, bks, davai, med, sabji, veg, kiryana, grc] = sd
 
+//toggle the category view to view results
 var catClick = (cat, sub) => {
     cat.addEventListener('click', () => {
         cat.classList.toggle("catOpen")
